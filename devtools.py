7643231bbfb07b3ad67f2ff39e5e82a0dca47c0d
dmitry-nazarov22 <dima.nazarov@icloud.com>
2026-02-18 11:02:26 +0200

    feat(devtools.py): correct switch: print instead of return

--- devtools.py
+++ devtools.py
@@ -4,15 +4,15 @@ import utils.password_generator as pg
 def switch(params):
     if len(params) > 1:
         if params[1] == "--hello":
-            return "Hello World!"
-        if params[1] == "--passwordgenerator" or "-pg":
+            print("Hello World!")
+        elif params[1] == "--passwordgenerator" or "-pg":
             pg.main()
         else:
-            return "Please input a valid command."
+            print("Please input a valid command.")
     else:
-        return "No command inserted."
+        print("No command inserted.")
 
 
 # Parameter array.
 params = sys.argv
-print(switch(params))
+switch(params)
