51fcf47de308572497213cd3f08ec9bfc2cae2ca
dmitry-nazarov22 <dima.nazarov@icloud.com>
2026-02-18 10:59:48 +0200

    feat(devtools.py): add passwordgenerator if statements

--- devtools.py
+++ devtools.py
@@ -1,9 +1,12 @@
 import sys
+import utils.password_generator as pg
 
 def switch(params):
     if len(params) > 1:
         if params[1] == "--hello":
             return "Hello World!"
+        if params[1] == "--passwordgenerator" or "-pg":
+            pg.main()
         else:
             return "Please input a valid command."
     else:
