2ea32d2f3eb0741c1f9b768fb8185ad021a7d933
Seppo Grubert <seppo.grubert@tuni.fi>
2026-02-19 13:21:06 +0200

    doc(pswd_strength_checker) Comment code

--- utils/pswd_strength_checker.py
+++ utils/pswd_strength_checker.py
@@ -8,22 +8,22 @@ badPasswords = {
 }
 
 print(f'Analysis for your password:')
-#check if password is in the common list
+
+#this will check if password is in the common list
 if password in badPasswords:
     print(f'Password is very common.')
 else:
     print(f'Password is not in the bad passwords list.')
-#
+
+# This will check for password length
+
 if len(password) < 6:
     print(f'Password is really short (less than 6 characters).')
 else:
     print(f'Looks ok length.')
 
-
+# this will check if the password contains numbers or special characters
 if (bool(re.search(r"(\d|[^A-Za-z0-9\s])", password))):
     print(f'Password contains special characters or numbers.')
 else:
     print(f'No special characters or numbers found.')
-
-# pswd_strength_checker
-
