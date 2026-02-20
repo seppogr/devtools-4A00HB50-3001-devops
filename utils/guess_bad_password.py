bafa56334eb631a58375c6a5e48d498a38dee143
Seppo Grubert <seppo.grubert@tuni.fi>
2026-02-20 16:11:59 +0200

    feat(guess_bad_password): Add hangman game logic

--- utils/guess_bad_password.py
+++ utils/guess_bad_password.py
@@ -3,17 +3,49 @@ import random
 #def main():
 name = input("What is your name? ")
 
-print(f'Good Luck {name}! Try to guess a bad password before time runs out.')
+print(f'Good Luck {name}!')
 
 badPasswords =[
     "admin", "password", "12345", "iloveyou", "qwerty",
     "abc123", "654321", "p@ssw0rd"
 ]
 
-word = random.choice(badPasswords)
+badPassword = random.choice(badPasswords)
 
 print("Guess the characters")
-print(f'{word}')
+print(f'Try to guess a bad password before time runs out.')
 
 guesses = ''
 turns = 12
+
+while turns > 0:
+
+    failed = 0
+
+    for char in badPassword:
+
+        if char in guesses:
+            print(char, end=" ")
+
+        else:
+            print("_")
+            failed += 1
+
+    if failed == 0:
+        print("You Win")
+        print("The bad password is: ", badPassword)
+        break
+
+    print()
+    guess = input("guess a character:")
+
+    guesses += guess
+
+    if guess not in badPassword:
+
+        turns -= 1
+        print("Wrong")
+        print("You have", + turns, 'more guesses')
+
+        if turns == 0:
+            print("You Loose")
